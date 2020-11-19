ae469c568b1d7ef82f5bac2895b0a45ff61a84dc
elitan <johan@eliasson.me>
2020-11-19 11:20:39 +0100

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM node:14.15.1-alpine3.10
 
 WORKDIR /usr/src/app
-RUN yarn add express node-dir
-RUN yarn add -D @babel/core @babel/cli @babel/preset-env @babel/plugin-transform-runtime
 COPY .babelrc .
 RUN mkdir src
 COPY index.js src/
--- index.js
+++ index.js
@@ -3,16 +3,38 @@ import dir from "node-dir";
 
 const app = express();
 
+const catchAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
+
 dir.files("./api", { sync: true }).forEach((file) => {
+  if (!file.endsWith(".js")) {
+    return;
+  }
+
   const endpoint = file
     .replace(/^api/, "")
     .replace(/\.js$/, "")
     .replace(/index$/, "");
   const func = require(`./${file}`);
   if ("default" in func) {
-    app.all(endpoint, func.default);
+    app.all(endpoint, catchAsync(func.default));
   } else {
-    app.all(endpoint, func);
+    app.all(endpoint, catchAsync(func));
+  }
+});
+
+app.use((err, req, res, next) => {
+  if (err) {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
   }
 });
 
