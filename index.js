63ce4ed08bc68a69691d0679ec061b594c3c9856
elitan <johan@eliasson.me>
2020-11-24 08:24:18 +0100

    update

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 import express from "express";
-import dir from "node-dir";
-
+import glob from "glob";
+import asyncHandler from "express-async-handler";
+import morgan from "morgan";
 const app = express();
 
-const catchAsync = (fn) => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
+app.disable("x-powered-by");
+app.use(morgan("tiny"));
 
-dir.files("./api", { sync: true }).forEach((file) => {
-  if (!file.endsWith(".js")) {
-    return;
-  }
-
+glob.sync("api/**/*.js").forEach((file) => {
+  console.log(file);
   const endpoint = file
     .replace(/^api/, "")
     .replace(/\.js$/, "")
     .replace(/index$/, "");
   const func = require(`./${file}`);
-  if ("default" in func) {
-    app.all(endpoint, catchAsync(func.default));
-  } else {
-    app.all(endpoint, catchAsync(func));
-  }
+  console.log({ endpoint });
+  app.all(endpoint, asyncHandler(func.default || func));
 });
 
 app.use((err, req, res, next) => {
@@ -37,7 +29,5 @@ app.use((err, req, res, next) => {
     });
   }
 });
-
-app.disable("x-powered-by");
 
 app.listen(process.env.PORT || 3010, () => console.log("Server ready"));
